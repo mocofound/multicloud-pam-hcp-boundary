c231901061a80210debe90676511428ea350ede3
Alex Harness <mocofound@gmail.com>
2022-05-17 13:40:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ terraform {
 
 // Configure the provider
 provider "hcp" {}
+
+resource "hcp_vault_cluster" "my_cluster" {
+  cluster_id = "hcp-tf-example-vault-cluster"
+  hvn_id = "main-hvn"
+  public_endpoint = true
+#  cloud_provider = "aws"
+  
+
+}
+
